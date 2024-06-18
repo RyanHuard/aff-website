330db63037464c051a950b158084d038edcb4eaf
Ryan Huard <ryan.huard@clearfunction.com>
2024-06-18 14:47:00 -0500

    Created MainLayout headers

--- frontend/src/components/layouts/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -9,30 +9,43 @@ import {
 
 import { NavLink, Outlet } from "react-router-dom";
 
+function TeamLogosHeader() {
+  return <div className="bg-black h-14"></div>;
+}
+
 function NavBar() {
   const navbarRoutes = ["Standings", "Schedule", "Stats", "Teams"];
 
   return (
-    <NavigationMenu className="max-w-full bg-aff-blue h-16 justify-start px-16">
-      <NavigationMenuList className="gap-4 font-semibold">
-        {navbarRoutes.map((route) => (
-          <NavigationMenuItem className="text-white font-sans">
-            <NavLink to={`/${route.toLowerCase()}`}>
-              <NavigationMenuLink>{route}</NavigationMenuLink>
-            </NavLink>
-          </NavigationMenuItem>
-        ))}
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="max-w-full bg-aff-blue h-16 px-16">
+      <NavigationMenu className="justify-start h-full max-w-7xl mx-auto">
+        <NavigationMenuList className="gap-6 font-semibold ">
+          {navbarRoutes.map((route) => (
+            <NavigationMenuItem className="text-white font-sans">
+              <NavLink to={`/${route.toLowerCase()}`}>
+                <NavigationMenuLink>{route}</NavigationMenuLink>
+              </NavLink>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
+}
+
+function StatLeadersHeader() {
+  return <div className="h-24 bg-slate-50"></div>;
 }
 
 export default function MainLayout() {
   return (
     <>
+      <TeamLogosHeader />
+      <StatLeadersHeader />
       <NavBar />
-
-      <Outlet />
+      <div className="mx-auto max-w-7xl">
+        <Outlet />
+      </div>
     </>
   );
 }
