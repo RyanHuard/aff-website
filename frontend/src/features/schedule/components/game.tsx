54ce0f24284432a15581fbddb534a8b6d22b3226
Ryan Huard <ryan.huard@clearfunction.com>
2024-06-26 15:29:29 -0500

    optional chainers

--- frontend/src/features/schedule/components/game.tsx
+++ frontend/src/features/schedule/components/game.tsx
@@ -7,19 +7,19 @@ export default function Game({ ...game }) {
       <div className="lg:mx-0 mb-4 flex h-14 items-center justify-center rounded-sm bg-white drop-shadow">
         <div className="flex  w-64 items-center justify-end">
           <span className="hidden px-1 sm:block">
-            ({game.away_team.current_season.wins}-
-            {game.away_team.current_season.loss})
+            ({game?.away_team?.current_season?.wins}-
+            {game?.away_team?.current_season?.loss})
           </span>
           <span className="hidden px-1 text-lg sm:block">
-            {game.away_team.name}
+            {game?.away_team?.name}
           </span>
           <div className="flex-col-reverse flex text-center mt-2">
             <span className="text-md px-2 sm:hidden">
-              {game.away_team.abbreviation}
+              {game?.away_team?.abbreviation}
             </span>
             <img
               className="mx-3 w-7 sm:w-11"
-              src={`/helmets/${game.away_team.helmet}`}
+              src={`/helmets/${game?.away_team?.helmet}`}
             />
           </div>
           <span
@@ -27,7 +27,7 @@ export default function Game({ ...game }) {
               game?.away_team_score == null && "hidden"
             } w-10 px-2 text-center text-lg font-bold sm:text-xl`}
           >
-            {game.away_team_score}
+            {game?.away_team_score}
           </span>
         </div>
 
@@ -35,19 +35,19 @@ export default function Game({ ...game }) {
 
         <div className="flex  w-64 flex-row-reverse items-center justify-end">
           <span className="hidden px-1 sm:block">
-            ({game.home_team.current_season.wins}-
-            {game.home_team.current_season.loss})
+            ({game?.home_team?.current_season.wins}-
+            {game?.home_team?.current_season.loss})
           </span>
           <span className="hidden px-1 text-lg sm:block">
-            {game.home_team.name}
+            {game?.home_team?.name}
           </span>
           <div className="flex-col-reverse flex text-center mt-2">
             <span className="text-md px-2 sm:hidden">
-              {game.home_team.abbreviation}
+              {game?.home_team?.abbreviation}
             </span>
             <img
               className="mx-3 w-7 sm:w-11"
-              src={`/helmets/${game.home_team.helmet}`}
+              src={`/helmets/${game?.home_team?.helmet}`}
             />
           </div>
           <span
