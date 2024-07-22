a44c9484e7c17aacd9c3c7378b6289a108f87d82
Ryan Huard <ryan.huard@clearfunction.com>
2024-07-22 11:00:57 -0400

    player page displays proper team

--- backend/api/players/player_handler.py
+++ backend/api/players/player_handler.py
@@ -8,15 +8,12 @@ def get_team_details_by_player(player_details):
         SELECT teams.*
         FROM teams
         JOIN player_stats ON player_stats.team_city = teams.abbreviation
-        WHERE player_stats.first_name = %s AND player_stats.last_name = %s
+        WHERE player_stats.first_name = %s AND player_stats.last_name = %s AND player_stats.season_id = %s
     """
 
     db.execute(
         query,
-        (
-            player_details["fname"],
-            player_details["lname"],
-        ),
+        (player_details["fname"], player_details["lname"], player_details["season_id"]),
     )
 
     team_details = db.fetchone()
