961dc35ff8f277853350157ee64c0360925aceb2
Ryan Huard <ryan.huard@clearfunction.com>
2024-07-29 21:59:30 -0400

    t

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -46,8 +46,7 @@ def create_app(test_config=None):
     app.config["MAIL_USE_TLS"] = True
     app.config["MAIL_USE_SSL"] = False
 
-    app.route("/")
-
+    @app.route("/")
     def index():
         return send_from_directory(app.static_folder, "index.html")
 
